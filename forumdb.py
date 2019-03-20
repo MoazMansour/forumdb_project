481a779bafe386831e061ed666ba0c2c90b58c45
Moaz Mansour <moaz.mansour@gmail.com>
2019-03-20 04:54:32 +0000

    Use sqldb to save and query posts

--- forumdb.py
+++ forumdb.py
@@ -1,15 +1,23 @@
 # "Database code" for the DB Forum.
 
 import datetime
+import psycopg2
 
 POSTS = [("This is the first post.", datetime.datetime.now())]
 
 def get_posts():
   """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+  conn = psycopg2.connect("dbname=forum")
+  cursor = conn.cursor()
+  cursor.execute('''select content, time from posts order by time desc;''')
+  results = cursor.fetchall()
+  conn.close()
+  return results
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
-
-
+  conn = psycopg2.connect("dbname=forum")
+  cursor = conn.cursor()
+  cursor.execute("insert into posts (content) values('{0}');".format(content))
+  conn.commit()
+  conn.close()
